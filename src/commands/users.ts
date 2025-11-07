cbf216d20a104c996f923858bbf0d6e599658ce2
horstrs <horst.marcelo@gmail.com>
2025-11-07 19:40:56 -0300

    Added register and reset users function

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,9 +1,44 @@
+//import { createUser } from "src/db/queries/users.js";
+import { createUser, getUserByName, resetUsers } from "../db/queries/users.js";
 import { setUser } from "../config.js";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
-  if (!args || args.length != 1) {
-    throw new Error("login command expects exactly one argument")
+export async function handlerLogin(cmdName: string, ...args: string[]): Promise<void> {
+  const [userName, others] = splitInput(args, cmdName);
+  if (!userName || others) {
+    throw new Error("Login command expects exactly one argument")
   }
-  setUser(args[0]);
-  console.log(`User ${args[0]} has been set.`)
+  const existingUser = await getUserByName(userName)
+  if (existingUser){
+    throw new Error(`User ${userName} not found`)
+  }
+  setUser(userName);
+  console.log(`User ${userName} has been set`)
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]): Promise<void> {
+  const [newName, others] = splitInput(args, cmdName);
+  if (!newName || others) {
+    throw new Error("Register command expects exactly one argument")
+  }
+  const newUser = await createUser(newName);
+  if(!newUser){
+    throw new Error("Error when creating the user");
+  }
+  setUser(newName);
+  console.log(`User ${newName} was created:`);
+  console.log(newUser);
+}
+
+export async function handlerReset(cmdName: string, ...args: string[]): Promise<void> {
+  await resetUsers();
+}
+
+function splitInput(receivedArgs: string[], cmdName: string): string[] {
+  
+  const [arg, _] = receivedArgs;
+  if (!arg){
+    console.log(`You need to provide at least one argument for command ${cmdName}.`);
+    process.exit(1);
+  }
+  return [arg, _];
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
 import type { CommandsRegistry } from "./commands/commands.js";
 import { registerCommand, runCommand } from "./commands/commands.js";
-import { handlerLogin } from "./commands/users.js";
+import { handlerLogin, handlerRegister, handlerReset } from "./commands/users.js";
 
-function main() {
+async function main() {
   
   const comRegistry:CommandsRegistry = {};
   registerCommand(comRegistry, "login", handlerLogin);
-  const [command, arg] = processInput(process.argv);
-  runCommand(comRegistry, command, arg);
+  registerCommand(comRegistry, "register", handlerRegister);
+  registerCommand(comRegistry, "reset", handlerReset);
+  const [command, args] = processInput(process.argv);
+  try {
+    await runCommand(comRegistry, command, args);
+  } catch (err) {
+    console.log("eeee laia");
+    console.log((err as Error).message);
+    process.exit(1);
+  }
+  process.exit(0);
 }
 
 function processInput(receivedArgs: string[]): string[] {
-  const [_, __, command, arg, ___] = receivedArgs;
-  if (!command || !arg){
-    console.log(`You need to provide a command with at least one argument.`);
+  const [_, __, command, args] = receivedArgs;
+  if (!command){
+    console.log('You need to provide a command.');
     process.exit(1);
   }
-  return [command, arg];
+  return [command, args];
 }
 
 main();
